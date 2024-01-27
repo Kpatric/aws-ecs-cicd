6c0ed4a2831341794ba40bfe5f0ec1281777b42e
Kpatric <patrickkimani273@gmail.com>
2024-01-27 10:06:56 +0300

    first commit

---
+++ src/main/java/com/pk/awsecscicd/AwsEcsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.pk.awsecscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEcsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsEcsCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pk/awsecscicd/AwsEcsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.awsecscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEcsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
